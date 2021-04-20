0bad77da19f6b307c3d55cc4cf8316b2388e2445
Kamelgasmi <kam_7@hotmail.fr>
2021-04-20 09:31:59 +0200

    1er commit

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('About', views.about, name='About'),
+    path('Services', views.services, name='Services'),
+    path('Contact', views.contact, name='Contact'),
+
 ]
